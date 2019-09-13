1e2904f6c82bc6013c6fefece0b2d31971ff9727
Connor Meth <connormeth27@gmail.com>
2019-09-13 21:46:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(array)
     puts string.capitalize + "!"
   end 
 end
-summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
+#summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
 
 def long_planeteer_calls# code an argument here
   # Your code here
