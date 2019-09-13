3680e2702731490a44c4719d0864c240454df848
Connor Meth <connormeth27@gmail.com>
2019-09-13 21:50:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ roll_call_dwarves(["Dopey", "Grumpy", "Bashful"])
 
 def summon_captain_planet(array)
   array.collect do |string|
-    puts (string.capitalize + "!").join
+    print string.capitalize + "!"
   end
   
 end
