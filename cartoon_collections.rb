9a7ec2d3277b4b38b93c7d173a0da23b5a6fe885
Connor Meth <connormeth27@gmail.com>
2019-09-13 21:59:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
-    return cheese if
+    return cheese if array.include?(cheese)
+  end 
 end
